7ddbe6070c5cc5195ffd77c12143e578854cc1ae
bouyahiasofiane72-tech <bouyahiasofiane72@gmail.com>
2026-09-02 11:38:41 +0100

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,34 @@
 package com.example
 
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
+class FrenchMangaProvider : MainAPI() {
+    override var mainUrl = "https://w16.french-manga.net"
+    override var name = "FrenchManga"
+    override val supportedTypes = setOf(TvType.Anime, TvType.AnimeMovie)
+    override var lang = "fr"
 
-    override var lang = "en"
+    override suspend fun search(query: String): ArrayList<SearchResponse> {
+        val link = "$mainUrl/?s=$query"
+        val html = app.get(link).document
+        val results = ArrayList<SearchResponse>()
 
-    // Enable this when your provider has a main page
-    override val hasMainPage = true
+        html.select("div.item").forEach { element ->
+            val title = element.selectFirst("a")?.attr("title") ?: return@forEach
+            val url = element.selectFirst("a")?.attr("href") ?: return@forEach
+            val poster = element.selectFirst("img")?.attr("src")
 
-    // This function gets called when you search for something
-    override suspend fun search(query: String): List<SearchResponse> {
-        return listOf()
+            results.add(
+                NewAnimeSearchResponse(
+                    title,
+                    url,
+                    TvType.Anime
+                ) {
+                    this.posterUrl = poster
+                }
+            )
+        }
+        return results
     }
 }
